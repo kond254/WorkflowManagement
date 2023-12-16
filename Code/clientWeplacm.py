d0409496bc6c3c222d101a24e916c59bc0c25f85
kond254 <diebestenschauen@hotmail.com>
2023-12-16 13:02:36 +0100

    Push Functional Client Weplacm

--- Code/clientWeplacm.py
+++ Code/clientWeplacm.py
@@ -2,22 +2,28 @@ from pyzeebe import ZeebeClient, create_insecure_channel
 
 class ClientWeplacm:
 
-    async def send_contract_to_weplacm(client: ZeebeClient):
-        await client.run_process(bpmn_process_id="Process_0yqlmow", #Process ID from WEPLACM
+    def __init__(self, contract_signed, capacity):
+        self.contract_signed = contract_signed
+        self.capacity=capacity
+        
+        
+    async def send_contract_to_weplacm(self, client: ZeebeClient):
+        await client.run_process(bpmn_process_id="Process_0yqlmow",  # Process ID from WEPLACM
                                 variables={
-                                "contract_signed": "True", #Boolean
-                                "capacity": "False", # //Boolean
-                                "compensation_per_worker": 0.1, # //double, 0-10 // 0.1 = 10%
-                                "type": "" # // Aufwand je nach Type unterschiedlich
+                                    "contract_signed": "True",  # Boolean
+                                    "capacity": "False",  # Boolean
+                                    "compensation_per_worker": 0.1,  # double, 0-10 // 0.1 = 10%
+                                    "type": ""  # Aufwand je nach Typ unterschiedlich
                                 }
-    )
+                                )
 
-    async def sendContract():
+    async def sendContract(self):
+        print("Start Send Contract to Weplacm")
         channel = create_insecure_channel(hostname="141.26.157.73", port=26500)
         print("Channel created to Weplacm") #
         client = ZeebeClient(channel)
         print("Client created to Weplacm") # 
-        await send_contract_to_weplacm(client)    
+        await self.send_contract_to_weplacm(client)    
 
 
 
