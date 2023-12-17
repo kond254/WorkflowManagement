6880aafb9ac7e4e4cf1e05bee898880559f558a0
iuolukzjct <jketterer@uni-koblenz.de>
2023-12-17 12:39:48 +0100

    BPMN and TestLoop

--- Code/worker.py
+++ Code/worker.py
@@ -14,23 +14,36 @@ def main():
     worker = ZeebeWorker(channel)
 
     @worker.task(task_type="sendContract")
-    async def send_contract(job: Job):
-        #cW.sendContract()  
-        jobType="jobType"
-        amountOfWorkers="amountOfW"
-        compensationPerWorker="compensationPerWorker"
-        print("NEEEW")
-        print(job.variables)
-        print(job.variables.values)
-        await cW.sendContract(jobType, amountOfWorkers, compensationPerWorker)
-        print("Contract send")
-
+    async def count_increase(job: Job, count: int):
+        print(count)
+        new_count = count+1
+        print(new_count)
+        await count_increase(new_count)
 
     loop = asyncio.get_event_loop()
     try:
         loop.run_until_complete(worker.work())
     except KeyboardInterrupt:
         pass
+
+    #@worker.task(task_type="sendContract")
+    #async def send_contract(job: Job):
+        #cW.sendContract()
+        #jobType="jobType"
+        #amountOfWorkers="amountOfW"
+        #compensationPerWorker="compensationPerWorker"
+        #print("NEEEW")
+        #print(job.variables)
+        #print(job.variables.values)
+        #await cW.sendContract(jobType, amountOfWorkers, compensationPerWorker)
+        #print("Contract send")
+
+
+    #loop = asyncio.get_event_loop()
+    #try:
+    #    loop.run_until_complete(worker.work())
+    #except KeyboardInterrupt:
+    #    pass
 
 
 if __name__ == '__main__':
