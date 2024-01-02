5d397e847aceb64134eb76eaff418f1c127e4bdd
iuolukzjct <jketterer@uni-koblenz.de>
2024-01-02 15:29:58 +0100

    SQL Querys

---
+++ Code/SQL/insertCandidatesIntoCandidateDB.sql
@@ -0,0 +1,3 @@
+INSERT INTO CandidateDB (ProcessID, first_name, last_name, gender, email, adress, 
+                        city, zip_code, country, age, previous_company, rating)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);
--- Code/db.py
+++ Code/db.py
@@ -47,3 +47,60 @@ class Databank:
             cur.execute(sql_script, tuple(data.values()))
             con.commit()
             print("DB INSERT EXECUTED")
+
+    def insert_candidates_in_db(self, process_id: int, first_name: str, last_name: str, gender: str, email: str, adress: str, city: str, zip_code: str, country: str, age: int, previous_company: str, rating: int):
+        with open('SQL/insertCandidateIntoCandidateDB.sql', 'r') as sql_file:
+            sql_script = sql_file.read()
+            cur = con.cursor()
+            
+            data = {
+            'job_process_instance_key': process_id,
+            'first_name': first_name,
+            'last_name': last_name,
+            'gender': gender,
+            'email': email,
+            'adress': adress,
+            'zip_code': zip_code,
+            'country': country,
+            'age': age,
+            'previous_company': previous_company,
+            'rating': rating
+            
+    }
+            cur.execute(sql_script, tuple(data.values()))
+            print("Candidate insertet into CandidateDB")
+            sql_query = "SELECT * FROM CandidateDB ORDER BY rating DESC;"
+            cur.execute(sql_query)
+            print("Candidates Sorted descending")
+            con.commit()
+    
+    def move_top10_candidates_into_topCandidateDB(self):
+        with con:
+            cur = con.cursor()
+
+            sql_query = "INSERT INTO TopCandidateDB SELECT * FROM CandidateDB LIMIT 10;"
+            cur.execute(sql_query)
+            sql_query = "DELETE FROM CandidateDB WHERE process_id = process_id LIMIT 10;"
+            cur.execute(sql_query)
+            con.commit()
+            print("Top 10 Candidates moved")
+
+
+    def remove_candidate_from_topCandidateDB(self, first_name: str, last_name: str):
+        with con:
+            cur = con.cursor()
+
+            sql_query = "DELETE FROM TopCandidateDB WHERE first_name = 'first_name' AND last_name = 'last_name';"
+            cur.execute(sql_query)
+            con.commit()
+            print("Candidate removed from TopCandidateDatabase")
+
+
+    def check_amount_of_candidates(self):
+        with con:
+            cur = con.cursor()
+
+            sql_query = "SELECT COUNT(*) FROM TopCandidateDB"
+            cur.execute(sql_query)
+            con.commit()
+            print(cur.execute(sql_query))
