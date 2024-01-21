426f29e923e13fc890331f1c331679df84f1e22b
fschumacher53 <fschumacher@uni-koblenz.de>
2024-01-21 18:11:56 +0100

    Card
    Imports + Card

--- GUI/src/app/app.module.ts
+++ GUI/src/app/app.module.ts
@@ -17,6 +17,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatListModule } from '@angular/material/list';
+import {MatProgressBarModule} from '@angular/material/progress-bar';
 import { MatSelectModule } from '@angular/material/select'; 
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -61,7 +62,9 @@ import { LoginComponent } from './login/login.component';
     MatCardModule,
     RouterModule,
     HttpClientModule,
-    MatSelectModule
+    MatSelectModule,
+    MatProgressBarModule
+
    
   ],
   providers: [],
