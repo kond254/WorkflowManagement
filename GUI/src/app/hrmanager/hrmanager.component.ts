895ccaf60920551da5befa08202055f7fc691c42
jokutsche <153863707+jokutsche@users.noreply.github.com>
2024-01-22 23:41:37 +0100

    Revert "dynamische hr department"
    This reverts commit 5954191fcb0bc31b8edca16862e9621e82e3f93b.

---
+++ GUI/src/app/hrmanager/hrmanager.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hrmanager',
+  templateUrl: './hrmanager.component.html',
+  styleUrl: './hrmanager.component.css'
+})
+export class HrmanagerComponent {
+
+}
