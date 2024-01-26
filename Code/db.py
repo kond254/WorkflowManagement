9d3db70b5ef1a76fecf2880116989dc016b20375
kond254 <diebestenschauen@hotmail.com>
2024-01-26 19:50:11 +0100

    addin comments

--- Code/db.py
+++ Code/db.py
@@ -4,23 +4,28 @@ import numpy as np
 con = sqlite3.connect("wbig.db", timeout=10)
 cur = con.cursor()
 
+
+#This class is the backend for the camunda worker
 class Databank:
 
     def __init__(self):
         # Aktiviere Fremdschlüssel-Unterstützung
         cur.execute("PRAGMA foreign_keys = ON")
 
-  
+    # insert Contract to System Table
     def insert_contract_in_systemdb(self, process_id: int, contract_signed: str, compensation: float):
         with con:
+            #execute sql statement if all important information
             cur.execute("INSERT INTO SystemDB (ProcessID, Contract, ContractSigned, Compensation) VALUES (" + str(process_id) +",\"\", "+str(contract_signed)+", "+str(compensation)+");")
             con.commit()
             print("DB INSERT EXECUTED")
             
-            
+    #insert job standards in the table
     def insert_job_standards_in_db(self, process_id: int, jobType: str, jobTitle:str, required_experience: int, job_description: str, responsibilities:str, location:str, job_mode:str, weekly_hours: int, pay: int, pto: int, benefits: str, industry:str, min_education_level:str, language:str, numberOfPositions: int):
+        #finding file in the sql folder
         with open('SQL/insertIntoJobStandards.sql', 'r') as sql_file:
             sql_script = sql_file.read()           
+            #generating json format
             data = {
             'job_process_instance_key': process_id,
             'jobTitle': jobTitle,
@@ -39,16 +44,17 @@ class Databank:
             'language': language,
             'numberOfPosition': numberOfPositions
         }
-
+            #create a tuple for json format that the sql file can read the data
             cur.execute(sql_script, tuple(data.values()))
             con.commit()
             print("DB INSERT EXECUTED")
 
+    #insert candidates in databank
     def insert_candidates_in_db(self, process_id: int, first_name: str, last_name: str, gender: str, email: str, linkedin: str, adress: str, city: str, zip_code: str, country: str, age: int, previous_company: str, rating: int):
         with open('SQL/insertCandidatesIntoCandidateDB.sql', 'r') as sql_file:
             sql_script = sql_file.read()
             cur = con.cursor()
-            
+            #creating json format for candidate
             data = {
             'job_process_instance_key': process_id,
             'first_name': first_name,
@@ -72,15 +78,16 @@ class Databank:
             print("Candidates Sorted descending")
             con.commit()
     
+    #get top 10 candidates from candidate table and insert them into top10candidate table
     def move_top10_candidates_into_topCandidateDB(self, process_id: int):
         with open('SQL/fillTopCandidateWIthRemainingCandidates.sql', 'r') as sql_file:
             sql_content = sql_file.read()
             #only one script at a time --> split
             sql_scripts = [s.strip() for s in sql_content.split(';') if s.strip()]
-            cur = con.cursor()
             x=0
             for sql_script in sql_scripts:
                 if sql_script.strip():
+                    #creating temp. tables the first only needs the process id
                     if x==0:
                         cur.execute(sql_script, (process_id,))
                     else:
@@ -90,23 +97,25 @@ class Databank:
             con.commit()
             print("Top 10 Candidates moved")
 
-
+    #remove candidate from top candidate db
     def remove_candidate_from_topCandidateDB(self, candidate_id:int):
+        #deletes the candidate from candidate db because candidate table and top candidate table are linked to one another 
         with open('SQL/deleteCandidateFromTopCandidateDB.sql', 'r') as sql_file:
             sql_content = sql_file.read()
             cur.execute(sql_content, (candidate_id,))
             con.commit()
             print("Candidate removed from TopCandidateDatabase")
 
-
+    #check amount of candidates in top candidate table for one unique process
     def check_amount_of_candidates_in_TopCandidateDB(self, process_id: int):
         with open('SQL/checkAmountTopCandidateDB.sql', 'r') as sql_file:
             sql_script = sql_file.read()
             cur.execute(sql_script, (process_id,))
             result = cur.fetchall()
-            
             return result
 
+
+    #check the amount of candidate in candidate table for one unique process
     def check_amount_of_candidates_in_CandidateDB(self, process_id: int):
         with open('SQL/checkAmountCandidateDB.sql', 'r') as sql_file:
             sql_script = sql_file.read()
@@ -114,7 +123,7 @@ class Databank:
             result = cur.fetchall()
             return result
             
-
+    #create an array for each entry for a unique process with candidates in top candidate table 
     def create_Array_for_MultiInstance(self, process_id: int):
         with open('SQL/createArrayForMultiInstance.sql', 'r') as sql_file:
             sql_script = sql_file.read()
@@ -127,7 +136,8 @@ class Databank:
                 TopCandidates.append(row[0])
 
             return TopCandidates
-        
+    
+    #joining top candidate table with candidate table to get data for each candidate in top candidate table 
     def Join_TopCandidate_with_CandidateDB(self, CandidateID: int):
         with open('SQL/joinTopCandidateWithCandidateDB.sql', 'r') as sql_file:
             sql_content = sql_file.read()
@@ -137,6 +147,7 @@ class Databank:
             con.commit()
             return data
         
+    #check if time slot is avaiable for each person
     def check_interview_dates(self, target_date:str, target_start_time:str, target_end_time:str):
         with open('SQL/checkCalendryEntry.sql', 'r') as sql_file:
             sql_content=sql_file.read()
@@ -150,8 +161,9 @@ class Databank:
                 return_var=True
             return return_var        
         
-    
+    #insert timeslot for each person
     def make_entry_in_calendar(self, target_date:str, target_start_time:str, target_end_time:str, event_name_candidate_id:int):
+        #first element in tuple is always the person id --> is hard coded because the id will remain the same only the person behind it changes through databank update
         data = [
             (1, 'Candidate Interview', target_date, target_start_time, target_end_time, event_name_candidate_id),
             (2, 'Candidate Interview', target_date, target_start_time, target_end_time, event_name_candidate_id),
@@ -159,6 +171,7 @@ class Databank:
         ]
         print(event_name_candidate_id)
         print(type(event_name_candidate_id))
+        #for each person insert the time for the interview
         for x in data:
             cur.execute(
                 """
@@ -170,7 +183,7 @@ class Databank:
         print('')
         con.commit()
         print('Committed')
-        
+        #safe time time for the candidate
         cur.execute(
         """
         UPDATE TopCandidate
@@ -182,6 +195,8 @@ class Databank:
         con.commit()
         print("Date in TopCandidate inserted")
         
+        
+        #getting the times and the time ids for each person
         cur.execute(
         """
         SELECT EventID FROM "Kalender"
@@ -194,13 +209,15 @@ class Databank:
         print(f'EventIDs: {single_tuple}')
         return single_tuple
         
-    def store_answer(self, InterviewAccepted: bool, CandidateID: int):
+    #store the answer for the candidate
+    def store_answer(self, InterviewAccepted: bool, candidate_id: int):
         with open('SQL/storeInterviewDateAnswer.sql', 'r') as sql_file:
             sql_content=sql_file.read()
-            cur.execute(sql_content, (InterviewAccepted, CandidateID))
+            cur.execute(sql_content, (InterviewAccepted, candidate_id))
             con.commit()
             print("DB Updated")
     
+    #return the amount of how many candidates answered and accapted the interview date and time 
     def checking_date_answers(self, process_id: int):
         with open('SQL/checkingDateAnswers.sql') as sql_file:
             sql_content=sql_file.read()
@@ -208,6 +225,7 @@ class Databank:
             result = cur.fetchall()
             return result
 
+    #delete top candidates that declined or did not answered the interview date
     def delete_TopCandidates(self, process_id: int):
         with open('SQL/deleteTopCandidatesDeclinedInterview.sql', 'r') as sql_file:
             sql_content=sql_file.read()
@@ -239,7 +257,7 @@ class Databank:
 
             return TopCandidates
             
-        
+    #return the candidates orderd by their interview dates 
     def order_by_interview(self, process_id: int):
         with open('SQL/orderByInterview.sql')as sql_file:
             sql_content=sql_file.read()
@@ -249,7 +267,7 @@ class Databank:
             print("Candidates Ordered")
             return result
 
-
+    #delete candidate from candidate table
     def delete_TopCandidate_due_Candidate_rejection(self, CandidateID: int):
          with open('SQL/deleteTopCandidateDueRejection.sql')as sql_file:
             sql_content=sql_file.read()
@@ -257,6 +275,7 @@ class Databank:
             con.commit()
             print("Candidate Deleted")
 
+    #store candidates answer if he accapted/declined the job
     def store_job_answer(self, JobAccepted: int, CandidateID: int):
         with open('SQL/storeFinalJobAnswer.sql', 'r') as sql_file:
             sql_content=sql_file.read()
@@ -264,6 +283,7 @@ class Databank:
             con.commit()
             print("DB Updated")
 
+    #delete all candidates that declined the job 
     def delete_TopCandidates_final(self, process_id: int):
         with open('SQL/deleteTopCandidatesDeclinedJob.sql', 'r') as sql_file:
             sql_content=sql_file.read()
@@ -271,6 +291,7 @@ class Databank:
             con.commit()
             print("Candidates Deleted")
 
+    #move candidates that accapted the job to the top employment table 
     def select_new_employees(self, process_id: int):
         with open('SQL/selectTopCandidates.sql', 'r') as sql_file:
             sql_content=sql_file.read()
@@ -315,20 +336,23 @@ class Databank:
             else:
                 print("Candidate List is Empty")
             
+    #check how many new employees are currently in this process
     def check_Count_new_employees(self, process_id: int):
         with open('SQL/checkAmountnewEmplyoees.sql', 'r') as sql_file:
             sql_content=sql_file.read()
             cur.execute(sql_content, (process_id, ))
             result = cur.fetchall()
-            return result[0][0]    
+            return result[0][0]    #return only the number
     
+    #check how many should be employed
     def check_number_of_positions(self, process_id: int):
         with open('SQL/selectNumberOfPositions.sql', 'r') as sql_file:
             sql_content=sql_file.read()
             cur.execute(sql_content, (process_id, ))
             result = cur.fetchall()
-            return result[0][0]
+            return result[0][0] #return only the number
     
+    #select the salary of new employes
     def check_annual_salary(self, process_id: int):
         with open('SQL/checkAnnualSalary.sql', 'r') as sql_file:
             sql_content=sql_file.read()
@@ -351,7 +375,8 @@ class Databank:
                 print(row[0])
             print(TopCandidates)
             return TopCandidates
-        
+    
+    
     def employed_candidates(self, process_id: int):
         with open('SQL/checkCandidatesEmployed.sql', 'r') as sql_file:
             sql_content=sql_file.read()
--- Code/worker.py
+++ Code/worker.py
@@ -29,90 +29,115 @@ def main():
     #
     #
 
-    
+    #send confirmation to Candidate about new job
     @worker.task(task_type="sendConfirmationToCandidate")
     async def send_confirmation_to_candidate(job: Job):
-        print("Job COnfirmation send")
+        print("-----Send Confirmation to Candidate-----")
+        print("Process Instance Key: " +str(job.process_instance_key))
         process_correlation_key=f"{job.process_instance_key}2I15"
         return {"process_correlation_key": process_correlation_key}
         
     
-    @worker.task(task_type="sendCandidateInterviewDate")
-    async def send_candidate_interview_date(job: Job):
-        print("Interview Date send") 
-
-
+    #Calculate the Evaluation of the 3 interviewer
     @worker.task(task_type="calculatingEvaluation")
     async def calculate_evaluation(job: Job, ratingHrManager: int, ratingHrRepresentive: int, ratingVP: int):
+        print("-----Calculate Interviewers Answers-----")
+        print("Process Instance Key: " +str(job.process_instance_key))
+        #calculate the final score
         finalScore = ratingHrManager + ratingHrRepresentive + ratingVP
+        # is the final score greater 20 let candidate pass
         if finalScore > 20:
             return{"finalSelectionPassed": True}
         else:
             return{"finalSelectionPassed": False}
         
+    #store the final Answer of Candidate
     @worker.task(task_type="storeFinalAnswer")
     async def store_final_answer(job: Job, CandidateID: int, JobAccepted: int):
+        print("-----Store the Job Answer for Candidate -----")
+        print("Process Instance Key: " +str(job.process_instance_key))
         db.store_job_answer(JobAccepted, CandidateID)
         
+        
+    # Delete candidate in table when he declined the job
     @worker.task(task_type="deleteTopCandidatesDeclinedJob")
     async def delete_topcandidates_declined_job(job: Job):
+        print("-----Delete Candidate-----")
+        print("Process Instance Key: " +str(job.process_instance_key))
         db.delete_TopCandidates_final(job.process_instance_key)    
             
+            
+    #check all final answers
     @worker.task(task_type="checkingFinalAnswers")
     async def checking_final_answers(job: Job):
+        print("-----Check all final Answers-----")
+        print("Process Instance Key: " +str(job.process_instance_key))
+        #get the amount of candidates in top candidate db. When one Candidate is in the table. We have atleast one new employee 
         amount = db.check_amount_of_candidates_in_TopCandidateDB(job.process_instance_key)
         if amount[0][0] > 0:
             return{"confirmations": True}
         else:
             return{"confirmations": False}
         
+    #Create new Employee
     @worker.task(task_type="moveCandidateToNewEmployee")
     async def move_candidates_to_new_employee(job: Job):
+        print("-----From Candidate to Employee-----")
+        print("Process Instance Key: " +str(job.process_instance_key))
         candidates = db.select_new_employees(job.process_instance_key)
         db.join_new_employee_data(candidates)
 
+
+    # Check how many Candidates where employed
     @worker.task(task_type="checkingEmployedCandidates")
     async def checking_employed_candidates(job: Job):
+        print("-----Employmend status of current process-----")
+        print("Process Instance Key: " +str(job.process_instance_key))
         newEmployeeCount = db.check_Count_new_employees(job.process_instance_key)
         numberOfpositions = db.check_number_of_positions(job.process_instance_key)
         if newEmployeeCount == numberOfpositions:
             return{"positionsFilled": True}
         else:
             return{"positionsFilled": False}
-        
-    @worker.task(task_type="receiveAnswer12")
-    async def test(job: Job):
-        print("Test")
-        x=int(f'{job.process_instance_key}2010')
-        print(x)
-        print(type(x))
-        return{"receiveAnswer": f'{job.process_instance_key}2010'}
-    
+
+    #sending WEPLACM info about new employment 
     @worker.task(task_type="sendWeplacmInfoEmployed")
     async def send_weplacm_info_employed(job: Job):
+        print("-----Sending information about new employees to WEPLACM-----")
+        print("Process Instance Key: " +str(job.process_instance_key))
         newEmployeeCount = db.check_Count_new_employees(job.process_instance_key)
         await cW.sendEmployeeAmount(newEmployeeCount)
         process_correlation_key=f"{job.process_instance_key}28"
         return {"process_correlation_key": process_correlation_key}
     
+    #Check invoice
     @worker.task(task_type="checkInvoice")
     async def check_invoice(job: Job, salarie_sum: int):
+        print("-----Check Invoice for correctness-----")
+        print("Process Instance Key: " +str(job.process_instance_key))
         newEmployeeCount = db.check_Count_new_employees(job.process_instance_key)
         Salary = db.check_annual_salary(job.process_instance_key)
+        #WEPLACM gets payed for each candidate we employed 
         CalculatedSalarySum = newEmployeeCount*Salary[0]*Salary[1]
+        #does the invoice add up to our callculations
         if CalculatedSalarySum == salarie_sum:
             return{"invoiceCorrect": True}
         else:
             return{"invoiceCorrect": False}
         
-    
+    #send weplacm info about wrong invoice 
     @worker.task(task_type="sendWeplacmInfoWrongInvoice")
     async def send_weplacm_info_wrong_invoice(job: Job):
+        print("-----Sending WEPLACM Information about wrong Invoice-----")
+        print("Process Instance Key: " +str(job.process_instance_key))
         await cW.sendFaultyInvoiceInfo()
         print("Faulty Invoice Info send")
 
+    #sending WEPLACM info about payment
     @worker.task(task_type="sendWeplacmInfoPayment")
     async def send_payment(job: Job):
+        print("-----Sending WEPLACM information that payment in on the way-----")
+        print("Process Instance Key: " +str(job.process_instance_key))
         await cW.sendPayment()
         print("Payment send")
 
