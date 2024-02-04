0009e2776eff957a42b506d73ddc347d2e83489f
jokutsche <153863707+jokutsche@users.noreply.github.com>
2024-02-04 13:45:17 +0100

    hash service fertig kommentiert

--- GUI/src/app/login.service.ts
+++ GUI/src/app/login.service.ts
@@ -4,23 +4,27 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 
-// Klasse ermöglicht einen boolean Wert zwischen Componenten auszutauschen (Login)
+//This Class enables a boolean and string value of the logged in user to be exchanged between components
 export class LoginService {
   private isLoggedIn: boolean = false;
   private userLoggedIn: string = '';
 
+  //Function to get the boolean value from the login (true/false)
   getloginValue(): boolean {
     return this.isLoggedIn;
   }
 
+  //Function to set the boolean value from the login (true/false)
   setloginValue(value: boolean): void {
     this.isLoggedIn = value;
   }
 
+  //Function to get the string value from the login (username)
   getloginUser(): string{
     return this.userLoggedIn;
   }
 
+  //Function to set the string value from the login (username)
   setloginUser(value: string): void {
     this.userLoggedIn = value;
   }
